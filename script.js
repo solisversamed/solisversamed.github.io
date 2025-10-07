cfbbe7e67e2e0c0ff08b6cb1ada46baecef16784
Enrique <jose.solis@versamedonline.com>
2025-10-07 16:55:51 -0400

    Scripts para asignar rutas a botones

---
+++ script.js
@@ -0,0 +1,19 @@
+
+document.onreadystatechange = () =>
+{
+    console.log("Documento ready");
+    const secciones = [{
+        id: "btn_seccion_2",
+        ruta: "seccion_2.html"
+    }]
+    
+    secciones.forEach(seccion => AsignarEvento(seccion))
+}
+
+function AsignarEvento(seccion){
+    let boton = document.querySelector("#" + seccion.id);
+    boton.addEventListener("click", () => {
+        console.log("click");
+         location.href = seccion.ruta
+    });
+}
---
+++ seccion2_controller.js
@@ -0,0 +1,13 @@
+document.onreadystatechange = () => {
+  const datosBotones = [{
+    id: "btn_1",
+    ruta: "Seccion_2/aprendiendo_css.html"
+  }];
+
+  datosBotones.forEach(datosBoton => {
+        let boton = document.querySelector("#" + datosBoton.id)
+        boton.addEventListener("click", ()=>{
+            location.href = datosBoton.ruta;
+        });
+    })
+}
