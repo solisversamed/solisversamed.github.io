49de37d28b1223a73587d3fe9cdc7cb8875cf46c
Enrique <jose.solis@versamedonline.com>
2025-10-22 09:34:02 -0400

    Quitando comentario

--- scripts/script.js
+++ scripts/script.js
@@ -42,31 +42,3 @@ document.onreadystatechange = async() =>
 
  
 }
-    
-
-//     console.log("Documento ready");
-//     const secciones = [
-//         {
-//             id: "btn_seccion_2",
-//             ruta: "paginas/seccion_2.html"
-//         },
-//         {
-//             id: "btn_seccion_3",
-//             ruta: "paginas/seccion_3.html"
-//         },
-//         {
-//             id: "btn_seccion_4",
-//             ruta: "paginas/seccion_4.html"
-//         }
-//     ]
-    
-//     secciones.forEach(seccion => AsignarEvento(seccion))
-// }
-
-// function AsignarEvento(seccion){
-//     let boton = document.querySelector("#" + seccion.id);
-//     boton.addEventListener("click", () => {
-//         console.log("click");
-//          location.href = seccion.ruta
-//     });
-// }
